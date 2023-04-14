8147725691147734ccf4f82a70f6cba9ebb2b660
Jakob Holst <jho@miracle.dk>
2023-04-14 16:05:17 +0200

    demo

--- demo.py
+++ demo.py
@@ -1,60 +1,78 @@
+#!/usr/bin/env python
+from PIL import Image
+import os
+# Load the image
 import pygame
 import datetime
 import time
 import random
 import numpy
 from PIL import Image, ImageDraw, ImageFont
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLabel, QApplication
+import sys
+import redis
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 
 
+def showimage(imagename):
+    app = QApplication(sys.argv)
+    label = QLabel()
+    pixmap = QPixmap(imagename)
+    label.setPixmap(pixmap)
+    label.show()
+    sys.exit(app.exec_())
+
+def randomplace(x, place, percentx):
+  if (place == "right"):
+    rangex = int(x - (x / 100 * percentx))
+    myx = random.randrange(rangex, x)
+
+  if (place == "left"):
+    rangex = int(x / 100 * percentx)
+    myx = random.randrange(0, rangex)
+  return myx
+
+def randangle():
+    randomangle = int(random.randrange(90))
+    return randomangle
 
 
-
-def genimage(mytext, mysize):
-  image = Image.new(mode="RGB", size=(800, 600), color=(0, 0, 0))
-  draw = ImageDraw.Draw(image)
-  font = ImageFont.truetype("arial.ttf", size=mysize)
-  text = mytext
-  text_box = draw.textbbox((0, 0), text, font=font)
-  x = (image.width - text_box[2]) // 2
-  y = (image.height - text_box[3]) // 2
-  draw.text((x, y), text, font=font, fill=( 255, 0, 0))
-  image.save("hello_pillow.png")
-  
-
-pygame.init()
-genimage(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), 60)
-screen = pygame.display.set_mode((1900, 1200))
-
-# Load the image
-image = pygame.image.load("hello_pillow.png")
-running = True
-number=0
-textsize=30
-textmin=20
-textmax=100
-textsizedir=True
-
-while running:
-        sincount = int(numpy.sin(number) * 30)
-        print(sincount)
-        rotated_image = pygame.transform.rotate(image,sincount ) 
-        screen.blit(rotated_image, (100, 100))
-        pygame.display.flip()
-        genimage(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), textsize)
-        add=random.randint(0,10)/10
-        if (textsizedir):
-             textsize = int(textsize + add)
-             if (textsize > textmax):
-                  textsizedir=False
-        else:
-             textsize = int(textsize - add)
-             if (textsize < textmin):
-                  textsizedir=True
-        
+def genpostit(mytext, myangle, mysize, filename):
+    bgimage = Image.open("postit.jpg")
+    draw = ImageDraw.Draw(bgimage)
+    font = ImageFont.truetype("arial.ttf", size=mysize)
+    text = mytext
+    text_box = draw.textbbox((0, 0), text, font=font)
+    x = (bgimage.width - text_box[2]) // 2
+    y = (bgimage.height - text_box[3]) // 2
+    draw.text((x, y), text, font=font, fill=(5, 44, 2))
+    bgimage = bgimage.resize((100, 100))
+    bgimage.save(filename)
 
 
-        image = pygame.image.load("hello_pillow.png")
-        number += 0.01
+def genmasterimage(imagename, x, y):
+    bgimage = Image.open("wedding.jpg")
+    redis_client.keys
+   # Image.Image.paste(image, bgimage, (0, 0))
+    for key in redis_client.scan_iter("*"):
+      postitid = key.decode()
+      myplacement = redis_client.hget(postitid, 'placement')
+      myplacement = myplacement.decode()
+      myfilename = redis_client.hget(postitid, 'filename')
+      myfilename = myfilename.decode()
+      mystatement = redis_client.hget(postitid, 'statement')
+      mystatement = mystatement.decode()
+      myx = redis_client.hget(postitid, 'x')
+      myx = int(myx.decode())
+      myy = redis_client.hget(postitid, 'y')
+      myy = int(myy.decode())
+      myimage = Image.open(myfilename)
+      Image.Image.paste(bgimage, myimage, (myx, myy))
+    bgimage.save(imagename)
 
-pygame.quit()
+genmasterimage("masterimage.png", 3840, 1080)
+showimage("masterimage.png")
+time.sleep(10)
