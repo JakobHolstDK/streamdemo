0cd5d25e913dc52e9d28b585f9548436c551890e
Jakob Holst <jho@miracle.dk>
2023-04-14 16:17:23 +0200

    demo

---
+++ demo copy.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import pygame
+import datetime
+import time
+import random
+import numpy
+from PIL import Image, ImageDraw, ImageFont
+
+
+
+
+
+
+def genimage(mytext, mysize):
+  image = Image.new(mode="RGB", size=(800, 600), color=(255, 255, 0))
+  draw = ImageDraw.Draw(image)
+  font = ImageFont.truetype("arial.ttf", size=mysize)
+  text = mytext
+  text_box = draw.textbbox((0, 0), text, font=font)
+  x = (image.width - text_box[2]) // 2
+  y = (image.height - text_box[3]) // 2
+  draw.text((x, y), text, font=font, fill=( 5, 44, 2))
+  image.save("hello_pillow.png")
+  
+
+pygame.init()
+genimage(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), 60)
+screen = pygame.display.set_mode((1900, 1200))
+
+# Load the image
+image = pygame.image.load("hello_pillow.png")
+running = True
+number=0
+textsize=30
+textmin=20
+textmax=100
+textsizedir=True
+
+while running:
+        sincount = int(numpy.sin(number) * 30)
+        print(sincount)
+        rotated_image = pygame.transform.rotate(image,sincount ) 
+        screen.blit(rotated_image, (100, 100))
+        pygame.display.flip()
+        genimage(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), textsize)
+        add=random.randint(0,10)/10
+        if (textsizedir):
+             textsize = int(textsize + add)
+             if (textsize > textmax):
+                  textsizedir=False
+        else:
+             textsize = int(textsize - add)
+             if (textsize < textmin):
+                  textsizedir=True
+        
+
+
+        image = pygame.image.load("hello_pillow.png")
+        number += 0.01
+
+pygame.quit()
---
+++ generatepostits.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+from PIL import Image
+import datetime
+import time
+import random
+from PIL import Image, ImageDraw, ImageFont
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLabel, QApplication
+import sys
+import redis
+import uuid
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+# List of possible statements
+
+myscreen = { "x": 2560, "y": 720, "right": 30, "left": 30 , "up": 90, "down": 90 , "fontsize": 40 }
+
+placements = [
+    "right",
+    "left"
+]
+person = {}
+person['left'] = [
+    "she",
+    "Mary",
+    "My sister",
+    "My mother",
+    "My aunt"
+    ]
+
+person['right'] = [
+    "he",
+    "Peter",
+    "My Brother",
+    "My father",
+    "My Cousin"
+    ]
+
+
+statements = [
+    "is an amazing dancer.",
+    "has a great sense of humor.",
+    "is a talented musician.",
+    "is an excellent cook.",
+    "has traveled to many countries.",
+    "is a natural leader.",
+    "is a great listener.",
+    "has a contagious smile.",
+    "is always up for an adventure.",
+    "has a heart of gold.",
+    "is a skilled writer.",
+    "is an exceptional athlete.",
+    "has a brilliant mind.",
+    "is a compassionate friend.",
+    "has a unique perspective.",
+    "is a gifted public speaker.",
+    "has overcome great challenges.",
+    "is a loyal companion.",
+    "is a visionary thinker.",
+    "is an inspiring role model.",
+    "has some massive knockers.",
+    "has huge mental problems"
+]
+
+def generatestatment():
+    myplace = random.choice(placements)
+    mypronome = random.choice(person[myplace])
+    mystatement = random.choice(statements)
+    print(mypronome + " " + mystatement)
+    mypayload = { "place": myplace, "statement": mypronome + ' ' + mystatement } 
+    return mypayload
+
+    
+#    print(pronome + ' ' + statement)
+
+def createpostit():
+    unique_id = str(uuid.uuid4())
+    epoch_time = time.time()
+    epoch_time_ns = int(epoch_time * 1e9)
+    postitid = str(epoch_time_ns).zfill(19)
+    unique_filename = 'postit_' + unique_id + '.jpg'
+    mystatement = generatestatment()
+    myrx = randomplace(mystatement['place'])
+    myry = randomplace('up')
+    bgimage = Image.open("postit.jpg")
+    draw = ImageDraw.Draw(bgimage)
+    font = ImageFont.truetype("arial.ttf", size=myscreen["fontsize"])
+    text = mystatement['statement']
+    text_box = draw.textbbox((0, 0), text, font=font)
+    x = (bgimage.width - text_box[2]) // 2
+    y = (bgimage.height - text_box[3]) // 2
+    draw.text((x, y), text, font=font, fill=(5, 44, 2))
+    bgimage = bgimage.resize((200, 200))
+    bgimage.save(unique_filename)
+    redis_client.hset(postitid, 'placement', mystatement["place"])
+    redis_client.hset(postitid, 'filename', unique_filename)
+    redis_client.hset(postitid, 'statement', text)
+    redis_client.hset(postitid, 'x', myrx)
+    redis_client.hset(postitid, 'y', myry)
+
+
+def randomplace(place):
+    if (place == "right"):
+        rangex = int(myscreen["x"] - (myscreen["x"] / 100 * myscreen["right"]))
+        myx = random.randrange(rangex, myscreen["x"])
+
+    if (place == "down"):
+        rangex = int(myscreen["y"] - (myscreen["y"] / 100 * myscreen["down"]))
+        myx = random.randrange(rangex, myscreen["y"])
+
+    if (place == "left"):
+        rangex = int(myscreen["x"] / 100 * myscreen["left"])
+        myx = random.randrange(0, rangex)
+
+    if (place == "up"):
+        rangex = int(myscreen["y"] / 100 * myscreen["up"])
+        myx = random.randrange(100, rangex) + 100
+    return myx
+
+
+def randangle():
+    randomangle = int(random.randrange(90))
+    return randomangle
+iterations = 30
+while iterations > 0:
+  createpostit()
+  iterations = iterations - 1
+
+
